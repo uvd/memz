e2438d3b2c9b1249d0d68829b89a4856f51c85ed
James Dowell <james@uvd.co.uk>
2017-10-09 12:45:28 +0100

    [web] Added steps within form

--- web/memz/src/app/Main.elm
+++ web/memz/src/app/Main.elm
@@ -2,45 +2,79 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
-import Date exposing (..)
-import Maybe exposing (..)
+import Html.Events exposing (onInput, onSubmit)
 
 
 type alias Model =
     { name : String
     , owner : String
     , endDateTime : String
+    , step : Step
     }
-    
-    
-type Msg =
-    Name String | Owner String | EndDateTime String
 
-        
+
+type Step
+    = NameStep
+    | OwnerStep
+    | EndDateTimeStep
+
+
+type Msg
+    = Name String
+    | Owner String
+    | EndDateTime String
+    | IncrementStep
+
+
 initialModel : Model
 initialModel =
     { name = ""
     , owner = ""
     , endDateTime = ""
+    , step = NameStep
     }
-    
+
 
 homePage model =
     p [] [ text "Long term memories made in real time" ]
 
 
-newEvent: Model -> Html Msg
+incrementCurrentStep : Step -> Step
+incrementCurrentStep step =
+    case step of
+        NameStep ->
+            OwnerStep
+
+        OwnerStep ->
+            EndDateTimeStep
+
+        s ->
+            s
+
+
+newEvent : Model -> Html Msg
 newEvent model =
-    div [] [
-        input [type_ "text", placeholder "Your name", value model.name, onInput Name] [],
-        input [type_ "text", placeholder "Event name", value model.owner, onInput Owner] [],
-        input [type_ "datetime-local", placeholder "Event date", value model.endDateTime] [],
-        p [] [text (model.name ++ " " ++ model.owner ++ model.endDateTime)]
-    ]
-    -- (case model.newEvent.name of
-    --   Nothing -> []
-    --   Just name -> [text name])]
+    div []
+        [ case model.step of
+            NameStep ->
+                Html.form [ onSubmit IncrementStep ]
+                    [ input [ type_ "text", placeholder "Your name", value model.name, onInput Name, required True ] []
+                    , input [ type_ "submit", value "Next" ] []
+                    ]
+
+            OwnerStep ->
+                Html.form [ onSubmit IncrementStep ]
+                    [ input [ type_ "text", placeholder "Event name", value model.owner, onInput Owner, required True ] []
+                    , input [ type_ "submit", value "Next" ] []
+                    ]
+
+            EndDateTimeStep ->
+                Html.form [ onSubmit IncrementStep ]
+                    [ input [ type_ "datetime-local", placeholder "Event date", value model.endDateTime, onInput EndDateTime, required True ] []
+                    , input [ type_ "submit", value "Next" ] []
+                    ]
+        , p [] [ text (model.name ++ " " ++ model.owner ++ model.endDateTime) ]
+        ]
 
 
 view : Model -> Html Msg
@@ -51,9 +85,21 @@ view =
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Name n -> {model | name = n}
-        Owner o -> {model | owner = o}
-        EndDateTime d -> {model | endDateTime = d}
+        Name n ->
+            { model | name = n }
+
+        Owner o ->
+            { model | owner = o }
+
+        EndDateTime d ->
+            { model | endDateTime = d }
+
+        IncrementStep ->
+            let
+                currentStep =
+                    model.step
+            in
+                { model | step = incrementCurrentStep currentStep }
 
 
 main =
