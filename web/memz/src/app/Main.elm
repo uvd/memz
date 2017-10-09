cad590d4f1f2d8f895ff5d03ff6d03f4e1d78afd
Ryan Hyslop <ryan@uvd.co.uk>
2017-10-09 11:29:11 +0100

    [web] Added first form field

--- web/memz/src/app/Main.elm
+++ web/memz/src/app/Main.elm
@@ -1,46 +1,63 @@
+module Main exposing (..)
+
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
 import Date exposing (..)
 import Maybe exposing (..)
 
+
 type alias Model =
-    {
-        newEvent :
-            {
-                name: Maybe String,
-                owner: Maybe String,
-                endDateTime: Maybe Date
-            }
+    { newEvent :
+        { name : Maybe String
+        , owner : Maybe String
+        , endDateTime : Maybe Date
+        }
     }
 
-type alias Msg = ()
 
+type Msg =
+    Name String
+
+
+initialModel : Model
 initialModel =
-    {
-        newEvent =
-            {
-                name = Nothing,
-                owner = Nothing,
-                endDateTime = Nothing
-            }
+    { newEvent =
+        { name = Nothing
+        , owner = Nothing
+        , endDateTime = Nothing
+        }
     }
+
 
 homePage model =
-    p [] [text "Long term memories made in real time"]
+    p [] [ text "Long term memories made in real time" ]
 
+
+newEvent: Model -> Html Msg
 newEvent model =
-    div [] []
+    div [] [
+        input [type_ "text", placeholder "Your name", value (Maybe.withDefault "" model.newEvent.name)]
+        []
+    ]
+    -- (case model.newEvent.name of
+    --   Nothing -> []
+    --   Just name -> [text name])]
+
 
 view : Model -> Html Msg
 view =
     newEvent
 
+
 update : Msg -> Model -> Model
-update msg model = model
+update msg model =
+    model
 
 
-main = Html.beginnerProgram
-    {
-        model = initialModel,
-        view = view,
-        update = update
-    }
+main =
+    Html.beginnerProgram
+        { model = initialModel
+        , view = view
+        , update = update
+        }
