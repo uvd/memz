37553de31ed31f5a922938162aef621ab4297c5f
Ryan Hyslop <ryan@uvd.co.uk>
2017-10-09 15:13:05 +0100

    [web] Put our big pants on with Program added a body encoder

--- web/memz/src/app/Main.elm
+++ web/memz/src/app/Main.elm
@@ -3,6 +3,8 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onSubmit)
+import Http exposing (Error)
+import Json.Encode as Encode
 
 
 type alias Model =
@@ -24,6 +26,11 @@ type Msg
     | Owner String
     | EndDateTime String
     | IncrementStep
+
+
+
+-- | CreateEvent
+-- | CreateEventResponse (Result Http.Error String)
 
 
 initialModel : Model
@@ -82,29 +89,62 @@ view =
     newEvent
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Name n ->
-            { model | name = n }
+            ( { model | name = n }, Cmd.none )
 
         Owner o ->
-            { model | owner = o }
+            ( { model | owner = o }, Cmd.none )
 
         EndDateTime d ->
-            { model | endDateTime = d }
+            ( { model | endDateTime = d }, Cmd.none )
 
         IncrementStep ->
             let
                 currentStep =
                     model.step
             in
-                { model | step = incrementCurrentStep currentStep }
+                ( { model | step = incrementCurrentStep currentStep }, Cmd.none )
 
 
+
+-- CreateEvent ->
+--     (model, postCreateEvent model)
+-- CreateEventResponse (Ok d)
+--     -> (model, Cmd.none)
+-- CreateEventResponse (Err d)
+--     -> (model, Cmd.none)
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initialModel
+    , Cmd.none
+    )
+
+
+bodyEncoder : { a | name : String, owner : String, endDateTime : String } -> String
+bodyEncoder data =
+    let
+        encodedValue =
+            Encode.object
+                [ 
+                    ( "name", Encode.string data.name ),
+                    ( "owner", Encode.string data.owner ),
+                    ( "endDateTime", Encode.string data.endDateTime )
+                ]
+    in
+        Encode.encode 0 encodedValue
+
+
+
+main : Program Never Model Msg
 main =
-    Html.beginnerProgram
-        { model = initialModel
+    program
+        { init = init
         , view = view
         , update = update
+        , subscriptions = (always Sub.none)
         }
