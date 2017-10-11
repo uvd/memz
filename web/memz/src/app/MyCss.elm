097c958b5f8e806cd3effa1948641a8b10fb723b
Ryan Hyslop <ryan@uvd.co.uk>
2017-10-11 10:00:53 +0100

    Initial attempt at styling

---
+++ web/memz/src/app/MyCss.elm
@@ -0,0 +1,49 @@
+module MyCss exposing (..)
+
+import Css exposing (..)
+import Css.Elements exposing (body, li)
+import Css.Namespace exposing (namespace)
+
+
+type CssClasses
+    = NavBar
+
+
+type CssIds
+    = Page
+
+
+css =
+    (stylesheet << namespace "dreamwriter")
+    [ body
+        [ overflowX auto
+        , minWidth (px 1280)
+        , fontFamilies ["sofia-pro"]
+        , textAlign center
+        , color (hex "0BC5C4")
+        , backgroundColor (rgb 255 255 255 )
+        ]
+    , id Page
+        [ backgroundColor (rgb 200 128 64)
+        , color (hex "CCFFFF")
+        , width (pct 100)
+        , height (pct 100)
+        , boxSizing borderBox
+        , padding (px 8)
+        , margin zero
+        ]
+    , class NavBar
+        [ margin zero
+        , padding zero
+        , children
+            [ li
+                [ (display inlineBlock) |> important
+                , color primaryAccentColor
+                ]
+            ]
+        ]
+    ]
+
+
+primaryAccentColor =
+    hex "ccffaa"
